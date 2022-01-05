d41229b449e78f7b19132cfe1af55b6843e987ae
Steven Arellano <stevenjarellano2@gmail.com>
2022-01-05 13:45:57 -0800

    guide

--- speed-typing/src/components/Guide.js
+++ speed-typing/src/components/Guide.js
@@ -2,5 +2,10 @@ import "./Guide.css";
 
 export default function Guide({ lessonNum }) {
     let lessonGuide = require("../guides/lesson" + lessonNum + ".gif");
-    return <img src={lessonGuide} alt="use whatever" className="guide-image" />;
+    return (
+        <div id="guide-container">
+            <div id="guide-title">Use The Following Keys</div>
+            <img src={lessonGuide} alt="use whatever" className="guide-image" />
+        </div>
+    );
 }
