c535e99db9f62b827018cf64fede67ebad07404c
Steven Arellano <stevenjarellano2@gmail.com>
2022-01-08 09:41:36 -0800

    new cap frequency

--- src/assets/typing.js
+++ src/assets/typing.js
@@ -15,7 +15,8 @@ export function cText(chars, numWords) {
     for (j = 0; j < numWords; j++) {
         text = "";
         for (i = 0; i < randomInt(3, 5); i++) {
-            let upperCase = Math.random() > 0.9 ? 1 : 0;
+            let first = !text.length;
+            let upperCase = (Math.random() > 0.80 && first) ? 1 : 0;
             let newChar = chars.charAt(
                 Math.floor(Math.random() * chars.length)
             );
